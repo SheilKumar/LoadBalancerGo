891381c9fe322d30cab42ae550ee8a67a9056772
sheilkumar <sk17@illinois.edu>
2022-04-03 07:19:16 +0100

    minimum viable implementation working

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
-import (
+import(
 	"LoadBalancerGo/loadbalancer"
+	"LoadBalancerGo/server"
 	"fmt"
+	"net/http"
+	"log"
+// )
 )
 
 func main() {
-	fmt.Println("test")
-	loadbalancer.PleaseWork()
+	lb := loadbalancer.LoadBalancer{}
+	// fmt.Println(lb)
+	// fmt.Println(sv1)
+	lb.AddServer(server.CreateNewServer("server-2", "http://127.0.0.1:6060"))
+	lb.AddServer(server.CreateNewServer("server-3", "http://127.0.0.1:6061"))
+	lb.AddServer(server.CreateNewServer("server-4", "http://127.0.0.1:6062"))
+	lb.AddServer(server.CreateNewServer("server-5", "http://127.0.0.1:6063"))
+	fmt.Println(lb)
+	http.HandleFunc("/", lb.ServeRequest)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
